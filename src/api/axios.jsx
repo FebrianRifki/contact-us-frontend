f1ec06ce086145d057014618f16839609fc24315
Febrianrifki <febrianrifki1702@gmail.com>
2025-06-22 19:28:00 +0700

    change env sample

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Buat instance Axios
 const api = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000/api',
+    baseURL: process.env.NEXT_APP_BACKEND_URL || 'http://localhost:8000/api',
     timeout: 10000,
     withCredentials: false,
     headers: {
